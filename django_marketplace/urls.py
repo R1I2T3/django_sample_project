a87d7547e3d76540d96fe2ca607dae4726dc6f7e
Ritesh jha <jhar27018@gmail.com>
2024-07-06 16:33:44 +0530

    edit and delete pages are created

--- django_marketplace/urls.py
+++ django_marketplace/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("items/", include("item.urls")),
+    path("dashboard/", include("dashboard.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
 from django.contrib.auth.decorators import login_required
-from .forms import NewItemForm
+from .forms import NewItemForm, UpdateItemForm
 # Create your views here.
 
 
@@ -27,3 +27,23 @@ def new(request):
     else:
         form = NewItemForm()
     return render(request, "item/form.html", {"form": form, "title": "New Item"})
+
+
+@login_required
+def update(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        form = UpdateItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect("item:detail", pk=pk)
+    else:
+        form = UpdateItemForm(instance=item)
+    return render(request, "item/form.html", {"form": form, "title": "Edit item"})
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    item.delete()
+    return redirect("/")
