e007c574a9e8efae0c253ae84ac8c90a916cbab1
Ritesh jha <jhar27018@gmail.com>
2024-07-06 15:30:25 +0530

    auth completed

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,18 @@
 from . import views
 from django.urls import path
+from django.contrib.auth.views import LoginView
+from .forms import LoginForm
 
-urlpatterns = [path("", views.index, name="home")]
+app_name = "core"
+urlpatterns = [
+    path("", views.index, name="home"),
+    path("contact/", views.contact, name="contact"),
+    path("signup/", views.signup, name="signup"),
+    path(
+        "login/",
+        LoginView.as_view(
+            authentication_form=LoginForm, template_name="core/login.html"
+        ),
+        name="login",
+    ),
+]
--- django_marketplace/settings.py
+++ django_marketplace/settings.py
@@ -137,3 +137,8 @@ NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# auth
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
